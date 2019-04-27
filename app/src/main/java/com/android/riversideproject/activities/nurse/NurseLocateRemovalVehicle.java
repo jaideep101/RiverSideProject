a6a04cbdd1bfafcef81b10386b224bf94cdeda28
JaideepSingh <jaideep.chauhan@niit-tech.com>
2019-04-27 17:40:07 +0530

    map display

--- app/src/main/java/com/android/riversideproject/activities/nurse/NurseLocateRemovalVehicle.java
+++ app/src/main/java/com/android/riversideproject/activities/nurse/NurseLocateRemovalVehicle.java
@@ -1,6 +1,7 @@
 package com.android.riversideproject.activities.nurse;
 
 import android.os.Bundle;
+import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
